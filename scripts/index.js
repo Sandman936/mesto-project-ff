a552fc6e1597f5a622211eeeca9feb5bf5ef1fde
Sandman <Sandman936@yandex.ru>
2024-01-26 07:30:01 +0300

    Работа на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,52 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
 
-// @todo: DOM узлы
+const cardsTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
+// DOM узлы
 
-// @todo: Функция удаления карточки
+const cardsList = document.querySelector(".places__list");
 
-// @todo: Вывести карточки на страницу
+const cardsItem = cardsTemplate.querySelector(".places__item");
+
+const cardImage = cardsTemplate.querySelector(".card__image");
+
+const cardTitle = cardsTemplate.querySelector(".card__title");
+
+
+// Функция создания карточки
+
+const renderCards = (name, link) => {
+    cardTitle.textContent = name;
+
+    cardImage.src = link;
+
+    cardImage.alt = name;
+
+    createCard();
+};
+
+const createCard = () => {
+    const cardsItemCloned = cardsItem.cloneNode(true);
+
+    cardsList.append(cardsItemCloned);
+
+    const removeButton = cardsItemCloned.querySelector(".card__delete-button");
+
+    removeButton.addEventListener("click", function (evt) {
+        removeCard(evt);
+    });
+}
+
+// Функция удаления карточки
+
+const removeCard = (evt) => {
+    const removeItem = evt.target;
+
+    removeItem.parentElement.remove();
+}
+
+// Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+    renderCards(card.name, card.link);
+});
